2701434f15792ad74489ff68463d307b4b90c694
Abhishek <abhishek2004abs@gmail.com>
2026-06-14 21:39:30 +0530

    database and api ready

--- backend/events/views.py
+++ backend/events/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Event
+from .serializers import EventSerializer
+
+
+class EventListView(APIView):
+    def get(self, request):
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+
+
+class EventDetailView(APIView):
+    def get(self, request, id):
+        event = Event.objects.get(id=id)
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
